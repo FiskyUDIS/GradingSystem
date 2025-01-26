84bbd3d7c9783d23ef6f08fbbcb3b42bbb1bf58e
Erik <erik.pitorak1@gmail.com>
2025-01-26 20:57:34 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColumnID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnPriezvisko = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnMeno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.buttonPridajStudenta = new System.Windows.Forms.Button();
+            this.buttonVymazStudenta = new System.Windows.Forms.Button();
+            this.labelVyberSiPredmet = new System.Windows.Forms.Label();
+            this.comboBoxSubjects = new System.Windows.Forms.ComboBox();
+            this.textBoxPridajPredmet = new System.Windows.Forms.TextBox();
+            this.labelPridajPredmet = new System.Windows.Forms.Label();
+            this.buttonPridajPredmet = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.buttonVymazPredmet = new System.Windows.Forms.Button();
+            this.comboBoxVymazPredmet = new System.Windows.Forms.ComboBox();
+            this.labelVymazPredmet = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.labelPredmet = new System.Windows.Forms.Label();
+            this.Column2ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2Priezvisko = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2Meno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2Znamka = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnID,
+            this.ColumnPriezvisko,
+            this.ColumnMeno});
+            this.dataGridView1.Location = new System.Drawing.Point(354, 5);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(409, 390);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // ColumnID
+            // 
+            this.ColumnID.HeaderText = "ID";
+            this.ColumnID.Name = "ColumnID";
+            this.ColumnID.ReadOnly = true;
+            // 
+            // ColumnPriezvisko
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Lime;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Red;
+            this.ColumnPriezvisko.DefaultCellStyle = dataGridViewCellStyle1;
+            this.ColumnPriezvisko.HeaderText = "Priezvisko";
+            this.ColumnPriezvisko.MinimumWidth = 6;
+            this.ColumnPriezvisko.Name = "ColumnPriezvisko";
+            this.ColumnPriezvisko.Width = 125;
+            // 
+            // ColumnMeno
+            // 
+            this.ColumnMeno.HeaderText = "Meno";
+            this.ColumnMeno.MinimumWidth = 6;
+            this.ColumnMeno.Name = "ColumnMeno";
+            this.ColumnMeno.Width = 125;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToOrderColumns = true;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2ID,
+            this.Column2Priezvisko,
+            this.Column2Meno,
+            this.Column2Znamka});
+            this.dataGridView2.Location = new System.Drawing.Point(136, 5);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(627, 390);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // buttonPridajStudenta
+            // 
+            this.buttonPridajStudenta.Location = new System.Drawing.Point(245, 16);
+            this.buttonPridajStudenta.Name = "buttonPridajStudenta";
+            this.buttonPridajStudenta.Size = new System.Drawing.Size(104, 23);
+            this.buttonPridajStudenta.TabIndex = 6;
+            this.buttonPridajStudenta.Text = "Pridaj studenta";
+            this.buttonPridajStudenta.UseVisualStyleBackColor = true;
+            this.buttonPridajStudenta.Click += new System.EventHandler(this.buttonPridajStudenta_Click);
+            // 
+            // buttonVymazStudenta
+            // 
+            this.buttonVymazStudenta.Location = new System.Drawing.Point(245, 45);
+            this.buttonVymazStudenta.Name = "buttonVymazStudenta";
+            this.buttonVymazStudenta.Size = new System.Drawing.Size(104, 23);
+            this.buttonVymazStudenta.TabIndex = 7;
+            this.buttonVymazStudenta.Text = "Vymaz studenta";
+            this.buttonVymazStudenta.UseVisualStyleBackColor = true;
+            this.buttonVymazStudenta.Click += new System.EventHandler(this.buttonVymazStudenta_Click);
+            // 
+            // labelVyberSiPredmet
+            // 
+            this.labelVyberSiPredmet.AutoSize = true;
+            this.labelVyberSiPredmet.Location = new System.Drawing.Point(7, 40);
+            this.labelVyberSiPredmet.Name = "labelVyberSiPredmet";
+            this.labelVyberSiPredmet.Size = new System.Drawing.Size(86, 13);
+            this.labelVyberSiPredmet.TabIndex = 8;
+            this.labelVyberSiPredmet.Text = "Vyber si Predmet";
+            // 
+            // comboBoxSubjects
+            // 
+            this.comboBoxSubjects.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSubjects.FormattingEnabled = true;
+            this.comboBoxSubjects.Location = new System.Drawing.Point(10, 56);
+            this.comboBoxSubjects.Name = "comboBoxSubjects";
+            this.comboBoxSubjects.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSubjects.TabIndex = 10;
+            this.comboBoxSubjects.SelectedIndexChanged += new System.EventHandler(this.comboBoxSubjects_SelectedIndexChanged);
+            // 
+            // textBoxPridajPredmet
+            // 
+            this.textBoxPridajPredmet.Location = new System.Drawing.Point(100, 217);
+            this.textBoxPridajPredmet.Name = "textBoxPridajPredmet";
+            this.textBoxPridajPredmet.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPridajPredmet.TabIndex = 11;
+            // 
+            // labelPridajPredmet
+            // 
+            this.labelPridajPredmet.AutoSize = true;
+            this.labelPridajPredmet.Location = new System.Drawing.Point(20, 220);
+            this.labelPridajPredmet.Name = "labelPridajPredmet";
+            this.labelPridajPredmet.Size = new System.Drawing.Size(74, 13);
+            this.labelPridajPredmet.TabIndex = 12;
+            this.labelPridajPredmet.Text = "Pridaj predmet";
+            // 
+            // buttonPridajPredmet
+            // 
+            this.buttonPridajPredmet.Location = new System.Drawing.Point(206, 217);
+            this.buttonPridajPredmet.Name = "buttonPridajPredmet";
+            this.buttonPridajPredmet.Size = new System.Drawing.Size(104, 23);
+            this.buttonPridajPredmet.TabIndex = 13;
+            this.buttonPridajPredmet.Text = "Pridaj";
+            this.buttonPridajPredmet.UseVisualStyleBackColor = true;
+            this.buttonPridajPredmet.Click += new System.EventHandler(this.buttonPridajPredmet_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 426);
+            this.tabControl1.TabIndex = 14;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttonVymazPredmet);
+            this.tabPage1.Controls.Add(this.comboBoxVymazPredmet);
+            this.tabPage1.Controls.Add(this.labelVymazPredmet);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.buttonPridajPredmet);
+            this.tabPage1.Controls.Add(this.buttonPridajStudenta);
+            this.tabPage1.Controls.Add(this.labelPridajPredmet);
+            this.tabPage1.Controls.Add(this.textBoxPridajPredmet);
+            this.tabPage1.Controls.Add(this.buttonVymazStudenta);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // buttonVymazPredmet
+            // 
+            this.buttonVymazPredmet.Location = new System.Drawing.Point(227, 248);
+            this.buttonVymazPredmet.Name = "buttonVymazPredmet";
+            this.buttonVymazPredmet.Size = new System.Drawing.Size(104, 23);
+            this.buttonVymazPredmet.TabIndex = 16;
+            this.buttonVymazPredmet.Text = "Vymaz";
+            this.buttonVymazPredmet.UseVisualStyleBackColor = true;
+            this.buttonVymazPredmet.Click += new System.EventHandler(this.buttonVymazPredmet_Click);
+            // 
+            // comboBoxVymazPredmet
+            // 
+            this.comboBoxVymazPredmet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxVymazPredmet.FormattingEnabled = true;
+            this.comboBoxVymazPredmet.Location = new System.Drawing.Point(100, 248);
+            this.comboBoxVymazPredmet.Name = "comboBoxVymazPredmet";
+            this.comboBoxVymazPredmet.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxVymazPredmet.TabIndex = 15;
+            // 
+            // labelVymazPredmet
+            // 
+            this.labelVymazPredmet.AutoSize = true;
+            this.labelVymazPredmet.Location = new System.Drawing.Point(20, 248);
+            this.labelVymazPredmet.Name = "labelVymazPredmet";
+            this.labelVymazPredmet.Size = new System.Drawing.Size(79, 13);
+            this.labelVymazPredmet.TabIndex = 14;
+            this.labelVymazPredmet.Text = "Vymaz predmet";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.labelPredmet);
+            this.tabPage2.Controls.Add(this.labelVyberSiPredmet);
+            this.tabPage2.Controls.Add(this.comboBoxSubjects);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // labelPredmet
+            // 
+            this.labelPredmet.AutoSize = true;
+            this.labelPredmet.Location = new System.Drawing.Point(7, 5);
+            this.labelPredmet.Name = "labelPredmet";
+            this.labelPredmet.Size = new System.Drawing.Size(46, 13);
+            this.labelPredmet.TabIndex = 11;
+            this.labelPredmet.Text = "Predmet";
+            // 
+            // Column2ID
+            // 
+            this.Column2ID.HeaderText = "ID";
+            this.Column2ID.Name = "Column2ID";
+            this.Column2ID.ReadOnly = true;
+            // 
+            // Column2Priezvisko
+            // 
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Lime;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Red;
+            this.Column2Priezvisko.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column2Priezvisko.HeaderText = "Priezvisko";
+            this.Column2Priezvisko.MinimumWidth = 6;
+            this.Column2Priezvisko.Name = "Column2Priezvisko";
+            this.Column2Priezvisko.ReadOnly = true;
+            this.Column2Priezvisko.Width = 125;
+            // 
+            // Column2Meno
+            // 
+            this.Column2Meno.HeaderText = "Meno";
+            this.Column2Meno.MinimumWidth = 6;
+            this.Column2Meno.Name = "Column2Meno";
+            this.Column2Meno.ReadOnly = true;
+            this.Column2Meno.Width = 125;
+            // 
+            // Column2Znamka
+            // 
+            this.Column2Znamka.HeaderText = "Znamky";
+            this.Column2Znamka.MinimumWidth = 6;
+            this.Column2Znamka.Name = "Column2Znamka";
+            this.Column2Znamka.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Student Grading";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button buttonPridajStudenta;
+        private System.Windows.Forms.Button buttonVymazStudenta;
+        private System.Windows.Forms.Label labelVyberSiPredmet;
+        private System.Windows.Forms.ComboBox comboBoxSubjects;
+        private System.Windows.Forms.TextBox textBoxPridajPredmet;
+        private System.Windows.Forms.Label labelPridajPredmet;
+        private System.Windows.Forms.Button buttonPridajPredmet;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label labelPredmet;
+        private System.Windows.Forms.Label labelVymazPredmet;
+        private System.Windows.Forms.ComboBox comboBoxVymazPredmet;
+        private System.Windows.Forms.Button buttonVymazPredmet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnPriezvisko;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnMeno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2Priezvisko;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2Meno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2Znamka;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, Dictionary<string, string>> subjectGrades = new Dictionary<string, Dictionary<string, string>>();
+        private string currentSubject;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateStudentIDs()
+        {
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (!dataGridView1.Rows[i].IsNewRow)
+                {
+                    dataGridView1.Rows[i].Cells["ColumnID"].Value = i + 1;
+                }
+            }
+        }
+
+        private void SyncNames()
+        {
+            if (currentSubject == null) return;
+            // Clear all rows in DataGridView2 before syncing
+            dataGridView2.Rows.Clear();
+
+            // Add names from DataGridView1 to DataGridView2, preserving any grades entered
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (!row.IsNewRow) // Skip the new row placeholder
+                {
+                    string id = row.Cells["ColumnID"].Value?.ToString();
+                    string priezvisko = row.Cells["ColumnPriezvisko"].Value?.ToString();
+                    string meno = row.Cells["ColumnMeno"].Value?.ToString();
+                    string fullName = $"{priezvisko} {meno}";
+
+                    // Find existing grade for this student (if any) using ID
+                    string existingGrade = "";
+                    if (subjectGrades.ContainsKey(currentSubject) && subjectGrades[currentSubject].ContainsKey(id))
+                    {
+                        existingGrade = subjectGrades[currentSubject][id]; // Get the existing grade
+                    }
+
+                    // Add the student to DataGridView2
+                    dataGridView2.Rows.Add(id, priezvisko, meno, existingGrade);
+                }
+            }
+        }
+
+        private void SaveCurrentSubjectGrades()
+        {
+            // Save grades from DataGridView2 to the dictionary for the current subject
+            var grades = new Dictionary<string, string>();
+
+            foreach (DataGridViewRow row in dataGridView2.Rows)
+            {
+                if (!row.IsNewRow)
+                {
+                    string id = row.Cells["Column2ID"].Value?.ToString();
+                    string priezvisko = row.Cells["Column2Priezvisko"].Value?.ToString();
+                    string meno = row.Cells["Column2Meno"].Value?.ToString();
+                    string grade = row.Cells["Column2Znamka"].Value?.ToString();
+
+                    grades[id] = grade; // Save grade by student ID
+                }
+            }
+
+            if (currentSubject != null)
+            {
+                subjectGrades[currentSubject] = grades;
+            }
+        }
+
+        private void LoadSubjectGrades(string subject)
+        {
+            if (subjectGrades.ContainsKey(subject))
+            {
+                var grades = subjectGrades[subject];
+
+                foreach (DataGridViewRow row in dataGridView2.Rows)
+                {
+                    if (!row.IsNewRow)
+                    {
+                        string id = row.Cells["Column2ID"].Value?.ToString();
+
+                        // Set the grade if it exists
+                        if (grades.ContainsKey(id))
+                        {
+                            row.Cells["Column2Znamka"].Value = grades[id];
+                        }
+                        else
+                        {
+                            row.Cells["Column2Znamka"].Value = ""; // Default empty grade
+                        }
+                    }
+                }
+            }
+        }
+
+        private void buttonPridajStudenta_Click(object sender, EventArgs e)
+        {
+            // Add an empty student row to DataGridView1
+            dataGridView1.Rows.Add("", "", "");
+
+            // Reassign IDs automatically
+            UpdateStudentIDs();
+            SaveCurrentSubjectGrades();
+            SyncNames(); // Sync names after adding a student
+        }
+
+        private void buttonVymazStudenta_Click(object sender, EventArgs e)
+        {
+            var aktivnaBunka = dataGridView1.CurrentCell;
+            if (aktivnaBunka != null)
+            {
+                int cisloRiadku = aktivnaBunka.RowIndex;
+                dataGridView1.Rows.RemoveAt(cisloRiadku);
+
+                // Reassign IDs automatically after removal
+                UpdateStudentIDs();
+                SyncNames(); // Sync names after removing a student
+            }
+        }
+
+        private void buttonPridajPredmet_Click(object sender, EventArgs e)
+        {
+            string predmet = textBoxPridajPredmet.Text;
+            if (!comboBoxSubjects.Items.Contains(predmet))
+            {
+                comboBoxSubjects.Items.Add(predmet);
+                comboBoxVymazPredmet.Items.Add(predmet);
+                subjectGrades[predmet] = new Dictionary<string, string>();
+            }
+            else
+            {
+                MessageBox.Show("Subject already exists.");
+            }
+
+            //currentSubject = predmet;
+
+        }
+
+        private void buttonVymazPredmet_Click(object sender, EventArgs e)
+        {
+            int index = comboBoxVymazPredmet.SelectedIndex;
+
+            if (index != -1)
+            {
+                string predmet = comboBoxVymazPredmet.Items[index].ToString();
+                comboBoxVymazPredmet.Items.RemoveAt(index);
+                comboBoxSubjects.Items.Remove(predmet);
+
+                if (subjectGrades.ContainsKey(predmet))
+                {
+                    subjectGrades.Remove(predmet);
+                }
+
+                if (currentSubject == predmet)
+                {
+                    currentSubject = null;
+                    dataGridView2.Rows.Clear();
+                    labelPredmet.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a subject to remove.");
+            }
+        }
+
+        private void comboBoxSubjects_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SaveCurrentSubjectGrades();
+
+            string selectedSubject = comboBoxSubjects.SelectedItem.ToString();
+            currentSubject = selectedSubject;
+
+            LoadSubjectGrades(currentSubject);
+            SyncNames();
+            labelPredmet.Text = currentSubject;
+        }
+    }
+}
